6682b0d1e93062607b2df080d9cf2a99f8cb8a25
Abhisek Datta <abhisek.datta@gmail.com>
2012-03-12 23:28:26 +0530

    --
    Signed-off-by: Abhisek Datta <abhisek.datta@gmail.com>

---
+++ lib/exporter/csv.rb
@@ -0,0 +1,26 @@
+module Exporter
+
+  class CSV
+    def initialize
+      @schema = []
+      @entries = []
+    end
+
+    def schema=(x)
+      @schema = x
+    end
+
+    def add_entry(a)
+      @entries << a
+    end
+
+    def to_string(sep = "\t")
+      r = String.new
+      r << @schema.collect {|e| e.capitalize}.join(sep) << "\n"
+      r << @entries.inject("") {|re, ee| re << ee.join(sep); re} << "\n"
+
+      r
+    end
+  end
+
+end
